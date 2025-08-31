e6f00e6fa871dcb773ddf186c08696286b258373
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-08-31 22:33:15 +0700

    Added a test case where a Conversation should have Messages in order

--- tests/conversation_test.rs
+++ tests/conversation_test.rs
@@ -20,4 +20,18 @@ mod tests {
         assert_eq!(conversation.messages.len(), 1);
         assert_eq!(conversation.messages[0].content, "Hello!");
     }
+
+    #[test]
+    fn message_should_in_order() {
+        let mut conversation = Conversation::new(1, vec![101, 202]);
+
+        let firstMessage = Message::new(1, 101, 202, "First");
+        let secondMessage = Message::new(2, 202, 101, "Second");
+
+        conversation.add_message(firstMessage);
+        conversation.add_message(secondMessage);
+
+        assert_eq!(conversation.messages[0].content, "First");
+        assert_eq!(conversation.messages[1].content, "Second");
+    }
 }
