f433c12bed7a6b557203c104ee479437827d29f1
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-09-07 23:14:37 +0700

    Added test case to unable to change name with an empty name

--- src/application/change_name.rs
+++ src/application/change_name.rs
@@ -7,7 +7,12 @@ impl ChangeNameUseCase {
         Self
     }
 
-    pub fn execute(&self, user: &mut User, new_name: &str) {
+    pub fn execute(&self, user: &mut User, new_name: &str) -> Result<(), String> {
+        if new_name.is_empty() {
+            return Err("New name cannot be empty".to_string());
+        }
+
         user.change_name(new_name);
+        Ok(())
     }
 }
--- tests/application/change_name_test.rs
+++ tests/application/change_name_test.rs
@@ -10,8 +10,20 @@ mod tests {
         let mut user = User::new(1, "Alice");
         let use_case = ChangeNameUseCase::new();
 
-        use_case.execute(&mut user, "Bob");
-
+        let result = use_case.execute(&mut user, "Bob");
+        
+        assert!(result.is_ok());
         assert_eq!(user.name, "Bob");
+    }
+
+    #[test]
+    fn change_name_cannot_with_empty_name() {
+        let mut user= User::new(1, "Alice");
+        let use_case = ChangeNameUseCase::new();
+
+        let result = use_case.execute(&mut user, "");
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "New name cannot be empty");
     }
 }
