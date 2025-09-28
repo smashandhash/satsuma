73180e6f4b96e48f53be1d964ea7af6dd788efa2
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-09-28 11:39:59 +0700

    Refactored the CreateConversationUseCaseTest by using  so it's readable and easier to maintain

--- tests/application/create_conversation_test.rs
+++ tests/application/create_conversation_test.rs
@@ -3,31 +3,27 @@ mod tests {
     use satsuma::application::create_conversation::CreateConversationUseCase;
     use satsuma::domain::conversation::Conversation;
     use satsuma::infrastructure::conversation_repository::ConversationRepository;
+    use rstest::rstest;
 
-    #[test]
-    fn create_conversation_between_two_users() {
+    #[rstest]
+    #[case("conversation between two users", 1, 2)]
+    #[case("conversation to self", 1, 1)]
+    fn create_conversation_between_two_users(
+        #[case] _label: &str,
+        #[case] sender_id: u64,
+        #[case] recipient_id: u64
+        ) {
         let mut repository = ConversationRepositoryStub::new();
         let mut use_case = CreateConversationUseCase::new(&mut repository);
 
-        let conversation = use_case.execute(1, 2).unwrap();
-        assert_eq!(conversation.participant_ids, vec![1, 2]);
-    }
-
-    #[test]
-    fn create_conversation_of_same_user() {
-        let mut repository = ConversationRepositoryStub::new();
-        let mut use_case = CreateConversationUseCase::new(&mut repository);
-        
-        let result = use_case.execute(1, 1).unwrap();
-
-        assert_eq!(result.participant_ids, [1, 1]);
+        let conversation = use_case.execute(sender_id, recipient_id).unwrap();
+        assert_eq!(conversation.participant_ids, vec![sender_id, recipient_id]);
     }
 
     #[test]
     fn create_conversation_failed_due_user_ids_are_zero() {
         let mut repository = ConversationRepositoryStub::new();
         let mut use_case = CreateConversationUseCase::new(&mut repository);
-
         let result = use_case.execute(1, 0);
 
         assert!(result.is_err());
