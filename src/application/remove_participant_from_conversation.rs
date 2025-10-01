ef645a307294ecefebe61ad281be9cca39442cc2
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-10-01 23:43:11 +0700

    Added test case for RemoveParticipantFromConversationUseCase, where the removal is only available for creator of the Conversation

--- src/application/remove_participant_from_conversation.rs
+++ src/application/remove_participant_from_conversation.rs
@@ -3,7 +3,11 @@ use crate::domain::conversation::Conversation;
 pub struct RemoveParticipantFromConversationUseCase;
 
 impl RemoveParticipantFromConversationUseCase {
-    pub fn execute(&self, conversation: &mut Conversation, actor_id: u64, target_id: u64) {
+    pub fn execute(&self, conversation: &mut Conversation, actor_id: u64, target_id: u64) -> Result<(), String> {
+        if conversation.creator_id != actor_id {
+            return Err("Only creator who can remove a participant.".to_string()); 
+        }
         conversation.remove_participant(target_id);
+        Ok(())
     }
 }
--- tests/application/remove_participant_from_conversation_test.rs
+++ tests/application/remove_participant_from_conversation_test.rs
@@ -10,8 +10,22 @@ mod tests {
         let mut conversation = Conversation::new(1, creator.id, vec![1, 2, 3]);
         let use_case = RemoveParticipantFromConversationUseCase;
 
-        use_case.execute(&mut conversation, 1, 2);
+        let result = use_case.execute(&mut conversation, 1, 2);
 
+        assert!(result.is_ok());
         assert_eq!(conversation.participant_ids, [1, 3]);
+    }
+
+    #[test]
+    fn non_creator_unable_to_remove_existing_participant() {
+        let creator = User::new(1, "Alice");
+        let non_creator = User::new(2, "Bob");
+        let mut conversation = Conversation::new(1, creator.id, vec![1, 2, 3]);
+        let use_case = RemoveParticipantFromConversationUseCase;
+
+        let result = use_case.execute(&mut conversation, non_creator.id, 3);
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "Only creator who can remove a participant.");
     }
 }
