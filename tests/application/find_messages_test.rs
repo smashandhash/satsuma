730fda2127a2510484fb533e5e2c357ad0b967e0
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-10-13 23:27:48 +0700

    Added test case for finding Message's content on a list of Conversation

---
+++ tests/application/find_messages_test.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod test {
+    use satsuma::domain::{
+        conversation::Conversation,
+        message::Message
+    };
+
+    #[test]
+    fn find_by_message_content() {
+    }
+}
