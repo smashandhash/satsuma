91f61b1368e2549a3e66e9fd462861eb6507e972
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-11-03 01:38:09 +0700

    Added test, when the given created_at parameter is out of range and too far in the future, then the DefaultTimestampValidator should throw an error of it

--- tests/domain/services/timestamp_validator_test.rs
+++ tests/domain/services/timestamp_validator_test.rs
@@ -5,6 +5,7 @@ mod tests {
         DefaultTimestampValidator,
         TimestampValidatorError
     };
+    use chrono::{ Duration, TimeZone, Utc };
 
     #[test]
     fn invalid_timestamp() {
@@ -14,5 +15,18 @@ mod tests {
 
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), TimestampValidatorError::InvalidTimestamp);
+    }
+
+    #[test]
+    fn timestamp_too_far_in_the_future() {
+        let now_timestamp = Utc::now().timestamp();
+        let now_datetime = Utc.timestamp_opt(now_timestamp, 0).unwrap();
+        let created_at_timestamp = now_datetime + Duration::seconds(60 * 6);
+        let created_at = created_at_timestamp.timestamp() as u64;
+        let sut = DefaultTimestampValidator;
+        let result = sut.validate_timestamp(created_at);
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), TimestampValidatorError::TimestampTooFarInTheFuture);
     }
 }
