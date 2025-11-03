c28f47c2d902dc73459eb416c4e6d92192256c43
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-11-03 18:43:23 +0700

    Refactored test cases like invalid timestamp for far in the future and too old

--- tests/domain/services/timestamp_validator_test.rs
+++ tests/domain/services/timestamp_validator_test.rs
@@ -6,6 +6,7 @@ mod tests {
         TimestampValidatorError
     };
     use chrono::{ Duration, TimeZone, Utc };
+    use rstest::rstest;
 
     #[test]
     fn invalid_timestamp() {
@@ -17,29 +18,22 @@ mod tests {
         assert_eq!(result.unwrap_err(), TimestampValidatorError::InvalidTimestamp);
     }
 
-    #[test]
-    fn timestamp_too_far_in_the_future() {
+    #[rstest]
+    #[case("Too far in the future", Duration::seconds(60 * 6), true, TimestampValidatorError::TimestampTooFarInTheFuture)]
+    #[case("Too old", Duration::days(8), false, TimestampValidatorError::TimestampTooOld)]
+    fn timestamp_validity_error(
+        #[case] _label: &str,
+        #[case] duration: Duration,
+        #[case] is_future: bool,
+        #[case] expected_error: TimestampValidatorError) {
         let now_timestamp = Utc::now().timestamp();
         let now_datetime = Utc.timestamp_opt(now_timestamp, 0).unwrap();
-        let created_at_timestamp = now_datetime + Duration::seconds(60 * 6);
+        let created_at_timestamp = if is_future { now_datetime + duration } else { now_datetime - duration };
         let created_at = created_at_timestamp.timestamp() as u64;
         let sut = DefaultTimestampValidator;
         let result = sut.validate_timestamp(created_at);
 
         assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), TimestampValidatorError::TimestampTooFarInTheFuture);
-    }
-
-    #[test]
-    fn timestamp_too_old() {
-        let now_timestamp = Utc::now().timestamp();
-        let now_datetime = Utc.timestamp_opt(now_timestamp, 0).unwrap();
-        let created_at_timestamp = now_datetime - Duration::days(8);
-        let created_at = created_at_timestamp.timestamp() as u64;
-        let sut = DefaultTimestampValidator;
-        let result = sut.validate_timestamp(created_at);
-
-        assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), TimestampValidatorError::TimestampTooOld);
+        assert_eq!(result.unwrap_err(), expected_error);
     }
 }
