8a39ca2f39ff16cc1883d970805cf2e4d950620f
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-11-03 16:31:26 +0700

    Added test for given timestamp is too old, then it should throw an error that explains it

--- tests/domain/services/timestamp_validator_test.rs
+++ tests/domain/services/timestamp_validator_test.rs
@@ -29,4 +29,17 @@ mod tests {
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), TimestampValidatorError::TimestampTooFarInTheFuture);
     }
+
+    #[test]
+    fn timestamp_too_old() {
+        let now_timestamp = Utc::now().timestamp();
+        let now_datetime = Utc.timestamp_opt(now_timestamp, 0).unwrap();
+        let created_at_timestamp = now_datetime - Duration::days(8);
+        let created_at = created_at_timestamp.timestamp() as u64;
+        let sut = DefaultTimestampValidator;
+        let result = sut.validate_timestamp(created_at);
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), TimestampValidatorError::TimestampTooOld);
+    }
 }
