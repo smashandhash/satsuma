14dc6fe27a087740aeb3316017b67280c1e34708
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-11-08 03:00:19 +0700

    Refactored SendMessageUseCase tests using repository instead of validating via NostrEventValidator. The next commit will delete the unused files.

--- tests/application/send_message_test.rs
+++ tests/application/send_message_test.rs
@@ -14,55 +14,58 @@ mod tests {
             }
         }
     };
-    use crate::helper::nostr_event_validator_stub::NostrEventValidatorStub;
+    use crate::helper::{
+        nostr_event_validator_stub::NostrEventValidatorStub,
+        message_repository_stub::MessageRepositoryStub
+    };
     use rstest::rstest;
 
-    #[test]
-    fn send_message_succeed() {
+    #[tokio::test]
+    async fn send_message_succeed() {
         let id = "id".to_string();
-        let max_length = 200;
         let content = "Hello, Bob.";
         let validator = NostrEventValidatorStub { simulated_error: None };
-        let sut = NostrSendMessageUseCase { max_length, validator };
+        let repository = MessageRepositoryStub::new(Vec::new());
+        let sut = NostrSendMessageUseCase { validator, repository };
 
         let message = Message::new(id, "".to_string(), content.to_string(), 0, 0, Vec::new(), "".to_string());
-        let result = sut.execute(message);
+        let result = sut.execute(&message).await;
 
         assert!(result.is_ok());
     }
 
     #[rstest]
-    #[case("rejected for empty message", 200, "", SendMessageUseCaseError::EmptyMessage)]
-    #[case("rejected for message has only spaces", 200, "   ", SendMessageUseCaseError::EmptyMessage)]
-    #[case("rejected for message is too long", 8, "Hello, Bob", SendMessageUseCaseError::MessageTooLong)]
-    fn send_message_error(
+    #[case("rejected for empty message", "", SendMessageUseCaseError::EmptyMessage)]
+    #[case("rejected for message has only spaces", "   ", SendMessageUseCaseError::EmptyMessage)]
+    #[tokio::test]
+    async fn send_message_error(
         #[case] _label: &str,
-        #[case] max_length: usize,
         #[case] content: &str,
         #[case] expected_error: SendMessageUseCaseError) {
         let id = "id".to_string();
         let validator = NostrEventValidatorStub { simulated_error: None };
-        let sut = NostrSendMessageUseCase { max_length, validator };
+        let repository = MessageRepositoryStub::new(Vec::new());
+        let sut = NostrSendMessageUseCase { validator, repository };
 
         let message = Message::new(id, "".to_string(), content.to_string(), 0, 0, Vec::new(), "".to_string());
-        let result = sut.execute(message);
+        let result = sut.execute(&message).await;
 
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), expected_error);
     }
 
-    #[test]
-    fn send_message_nostr_error() {
+    #[tokio::test]
+    async fn send_message_nostr_error() {
         let simulated_error = NostrEventValidatorError::TimestampError(TimestampValidatorError::InvalidTimestamp);
         let id = "id".to_string();
-        let max_length = 200;
         let content = "Hello, Bob.";
         let validator = NostrEventValidatorStub { simulated_error: Some(simulated_error.clone()) };
+        let repository = MessageRepositoryStub::new(Vec::new());
         let expected_error = SendMessageUseCaseError::NostrError(simulated_error.clone());
-        let sut = NostrSendMessageUseCase { max_length, validator };
+        let sut = NostrSendMessageUseCase { validator, repository };
 
         let message = Message::new(id, "".to_string(), content.to_string(), 0, 0, Vec::new(), "".to_string());
-        let result = sut.execute(message);
+        let result = sut.execute(&message).await;
 
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), expected_error);
