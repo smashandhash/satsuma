7ad7b5d742f5b37d9384c080b9c3215d9a6c05fc
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-11-15 23:38:33 +0700

    Work in progress for refactoring the Conversation struct. Will be fixing the involved files that uses Conversation struct.

--- src/domain/conversation.rs
+++ src/domain/conversation.rs
@@ -1,47 +1,30 @@
-use chrono::{DateTime, Utc};
-
-use super::message::Message;
+use crate::domain::message::Message;
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Conversation {
-    pub id: u64,
-    pub creator_public_key: String,
-    pub participant_public_keys: Vec<String>,
+pub struct ChatSession {
+    pub id: String,
+    pub context: ChatSessionContext,
     pub messages: Vec<Message>,
-    pub created_at: DateTime<Utc>,
 }
 
-impl Conversation {
-    pub fn new(id: u64, creator_public_key: &str, participant_public_keys: Vec<&str>) -> Self {
+#[derive(Debug, Clone, PartialEq)]
+pub enum ChatSessionContext {
+    Direct(String),
+    Group(String),
+    Channel(String),
+    Thread(String),
+}
+
+impl ChatSession {
+    pub fn new(id: String, context: ChatSessionContext) -> Self {
         Self {
             id,
-            creator_public_key: creator_public_key.to_string(),
-            participant_public_keys: participant_public_keys.into_iter().map(|key| key.to_string()).collect(),
+            context,
             messages: Vec::new(),
-            created_at: Utc::now(),
         }
     }
 
-    pub fn add_message(&mut self, message: Message) -> bool {
-        let valid_sender = self.participant_public_keys.contains(&message.public_key);
-        
-        if valid_sender {
-            self.messages.push(message);
-            true
-        } else {
-            false
-        }
-    }
-
-    pub fn add_participant(&mut self, user_public_key: String) -> Result<(), String> {
-        if self.participant_public_keys.contains(&user_public_key) {
-            return Err("A `Conversation` cannot contain duplicate participants.".to_string());
-        }
-        self.participant_public_keys.push(user_public_key);
-        Ok(())
-    }
-
-    pub fn remove_participant(&mut self, user_public_key: String) {
-        self.participant_public_keys.retain(|target_public_key| *target_public_key != user_public_key);
+    pub fn add_message(&mut self, message: Message) {
+        self.messages.push(message);
     }
 }
