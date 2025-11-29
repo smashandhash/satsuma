1132afa7053865c236bf51a47d7fd27a9ea36fb9
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-11-29 23:36:11 +0700

    Work in progress refactoring the get_conversation_messages file.

--- src/application/get_conversation_messages.rs
+++ src/application/get_conversation_messages.rs
@@ -1,14 +1,31 @@
 use crate::{
-    domain::message::Message,
-    infrastructure::message_repository::MessageRepository
+    domain::{
+        message::Message,
+        chat_session::ChatSessionContext,
+    },
+    infrastructure::chat_container_repository::{
+        ChatContainerRepository,
+        ChatContainerRepositoryError
+    },
 };
 
-pub struct GetConversationMessagesUseCase<R: MessageRepository> {
+pub trait LoadChatContainerMessagesUseCase {
+    fn execute(&self, chat_container_id: String) -> Result<Vec<Message>, LoadChatContainerMessagesUseCaseError>;
+}
+
+pub struct LoadChatContainerMessagesUseCaseImplementation<R: ChatContainerRepository> {
     pub repository: R,
 }
 
-impl<R: MessageRepository> GetConversationMessagesUseCase<R> {
-    pub fn execute(&self, sender_public_key: String, recipient_public_key: String) -> Vec<Message> {
-        self.repository.find_conversation(sender_public_key, recipient_public_key)
+impl<R: ChatContainerRepository> LoadChatContainerMessagesUseCase for LoadChatContainerMessagesUseCaseImplementation<R> {
+    fn execute(&self, chat_container_id: String) -> Result<Vec<Message>, LoadChatContainerMessagesUseCaseError> {
+        let chat_container = self.repository.load(chat_container_id).map_err(|e| LoadChatContainerMessagesUseCaseError::RepositoryError(e))?;
+
+        Ok(chat_container.sessions.into_iter().map(|session| { session.context == ChatSessionContext::Root }))
     }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum LoadChatContainerMessagesUseCaseError {
+    RepositoryError(ChatContainerRepositoryError),
 }
