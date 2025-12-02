18fb8c36e811b697178e26ef7df6926eabc2f82a
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-12-02 23:28:55 +0700

    Renamed load_chat_container_messages file into load_messages for simplicity

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,7 +1,7 @@
 pub mod register_user;
 pub mod change_name;
 pub mod send_message;
-pub mod load_chat_container_messages;
+pub mod load_messages;
 pub mod create_direct_chat;
 pub mod add_participants_to_chat_container;
 pub mod remove_participants_from_chat_container;
