7c685f52b1a6ea26b90107dde373f5403da6cb9d
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-12-06 21:14:39 +0700

    Fixed for inproper implementation of LoadMessagesUseCase.

--- src/application/load_messages.rs
+++ src/application/load_messages.rs
@@ -1,40 +1,62 @@
 use crate::{
-    domain::{
-        message::Message,
-        chat_session::ChatSessionContext,
-    },
-    infrastructure::chat_container_repository::{
-        ChatContainerRepository,
-        ChatContainerRepositoryError
+    domain::message::Message,
+    infrastructure::{
+        local_storage::LocalStorage,
+        key_provider::{
+            KeyProvider,
+            KeyProviderError,
+        },
+        chat_container_repository::{
+            ChatContainerRepository,
+            ChatContainerRepositoryError,
+        },
+        message_repository::{
+            MessageRepository,
+            MessageRepositoryError,
+        },
     },
 };
+use async_trait::async_trait;
+use std::sync::Arc;
+use nostr_sdk::prelude::*;
 
+#[async_trait]
 pub trait LoadMessagesUseCase {
-    fn execute(&self, chat_container_id: String) -> Result<Vec<Message>, LoadChatContainerMessagesUseCaseError>;
+    async fn execute(&self, chat_container_id: String) -> Result<Vec<Message>, LoadMessagesUseCaseError>;
 }
 
-pub struct LoadMessagesUseCaseImplementation<R: ChatContainerRepository> {
-    pub repository: R,
+pub struct LoadMessagesUseCaseImplementation<S: LocalStorage, K: KeyProvider, CR: ChatContainerRepository, MR: MessageRepository> {
+    pub storage: Arc<S>,
+    pub provider: Arc<K>,
+    pub container_repository: Arc<CR>,
+    pub message_repository: Arc<MR>
 }
 
-impl<R: ChatContainerRepository> LoadMessagesUseCase for LoadMessagesUseCaseImplementation<R> {
-    fn execute(&self, chat_container_id: String) -> Result<Vec<Message>, LoadChatContainerMessagesUseCaseError> {
-        let chat_container = self.repository.load(chat_container_id).map_err(|e| LoadChatContainerMessagesUseCaseError::RepositoryError(e))?;
+#[async_trait]
+impl<S: LocalStorage, K: KeyProvider, CR: ChatContainerRepository, MR: MessageRepository> LoadMessagesUseCase for LoadMessagesUseCaseImplementation<S, K, CR, MR> where S: LocalStorage + Send + Sync, K: KeyProvider + Send + Sync, CR: ChatContainerRepository + Send + Sync, MR: MessageRepository + Send + Sync {
+    async fn execute(&self, chat_container_id: String) -> Result<Vec<Message>, LoadMessagesUseCaseError> {
+        let secret_key = self
+            .storage
+            .load_secret_key()
+            .map_err(|e| LoadMessagesUseCaseError::Unauthorized(e.to_string()))?;
 
-        // TODO: Load from ChatSessions
-        // TODO: Load from MessageRepository
+        let keys = self.provider.parse_secret_key(&secret_key)
+            .await
+            .map_err(|e| LoadMessagesUseCaseError::InvalidKey(e))?;
 
-        let messages = &chat_container.sessions
-            .iter()
-            .find(|session| session.context == ChatSessionContext::Root )
-            .unwrap()
-            .messages;
+
+        let chat_container = self.container_repository.load(chat_container_id.clone()).map_err(|e| LoadMessagesUseCaseError::ContainerRepositoryError(e))?;
+
+        let messages = self.message_repository.find_root_messages(chat_container_id.clone(), chat_container.context, keys.public_key().to_bech32().unwrap()).await.map_err(|e| LoadMessagesUseCaseError::MessageRepositoryError(e))?;
 
         Ok(messages.clone())
     }
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum LoadChatContainerMessagesUseCaseError {
-    RepositoryError(ChatContainerRepositoryError),
+pub enum LoadMessagesUseCaseError {
+    Unauthorized(String),
+    InvalidKey(KeyProviderError),
+    ContainerRepositoryError(ChatContainerRepositoryError),
+    MessageRepositoryError(MessageRepositoryError),
 }
