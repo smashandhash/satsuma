b561b41b790b67c0e330da3efb37a67fb7d7720b
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-12-14 17:02:55 +0700

    Added test case for repository error when searching chat on SearchChatUseCase.

--- tests/application/search_chat_test.rs
+++ tests/application/search_chat_test.rs
@@ -1,9 +1,12 @@
 #[cfg(test)]
 mod tests {
-    use satsuma::application::search_chat::{
-        SearchChatUseCase,
-        SearchChatUseCaseImplementation,
-        SearchChatUseCaseError,
+    use satsuma::{
+        application::search_chat::{
+            SearchChatUseCase,
+            SearchChatUseCaseImplementation,
+            SearchChatUseCaseError,
+        },
+        infrastructure::chat_container_repository::ChatContainerRepositoryError,
     };
     use crate::helper::chat_container_repository_stub::ChatContainerRepositoryStub;
     use std::sync::Arc;
@@ -16,5 +19,17 @@ mod tests {
         let result = sut.execute("keyword".to_string());
 
         assert!(result.is_ok());
+    }
+
+    #[test]
+    fn repository_error_failed_to_search_chat() {
+        let error = ChatContainerRepositoryError::ContainerNotFound;
+        let repository = Arc::new(ChatContainerRepositoryStub { simulated_error: Some(error.clone()), mocked_chat_container: None });
+        let sut = SearchChatUseCaseImplementation::new(repository);
+
+        let result = sut.execute("keyword".to_string());
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), SearchChatUseCaseError::RepositoryError(error.clone()));
     }
 }
