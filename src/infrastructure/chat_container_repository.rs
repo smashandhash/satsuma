edc006d8321fb8cc1153724396a4cdc309ea855d
Gogo <226705116+smashandhash@users.noreply.github.com>
2025-12-15 22:58:15 +0700

    In progress implementing the send, load, and search functions of the ChatContainerRepository.

--- src/infrastructure/chat_container_repository.rs
+++ src/infrastructure/chat_container_repository.rs
@@ -6,6 +6,19 @@ pub trait ChatContainerRepository {
     fn search(&self, keyword: String) -> Result<Vec<ChatContainer>, ChatContainerRepositoryError>;
 }
 
+pub struct NostrChatContainerRepository;
+
+impl ChatContainerRepository for NostrChatContainerRepository {
+    fn save(&self, container: ChatContainer) -> Result<(), ChatContainerRepositoryError> {
+    }
+
+    fn load(&self, container_id: String) -> Result<ChatContainer, ChatContainerRepositoryError> {
+    }
+
+    fn search(&self, keyword: String) -> Result<Vec<ChatContainer>, ChatContainerRepositoryError> {
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum ChatContainerRepositoryError {
     ContainerNotFound,
