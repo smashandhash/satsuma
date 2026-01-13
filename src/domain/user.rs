1a29e53abb50380066b8ad7f1e46cbec89af52b9
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-01-13 23:41:44 +0700

    Added test case for getting a user profile based on the profile_id

--- src/domain/user.rs
+++ src/domain/user.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct User {
     pub public_key: String,
     pub name: String,
--- tests/application/get_profile_test.rs
+++ tests/application/get_profile_test.rs
@@ -1,7 +1,26 @@
 #[cfg(test)]
 mod tests {
+    use satsuma::domain::user::User;
+
+    pub trait GetProfileUseCase {
+        fn execute(&self, profile_id: String) -> User;
+    }
+
+    pub struct GetProfileUseCaseImplementation;
+
+    impl GetProfileUseCase for GetProfileUseCaseImplementation {
+        fn execute(&self, _profile_id: String) -> User {
+            User::new("public_key", "name")
+        }
+    }
+
     #[test]
     fn get_profile() {
-        // TODO: Still think what to do
+        let profile_id = "profile_id".to_string();
+        let sut = GetProfileUseCaseImplementation;
+        let result = sut.execute(profile_id);
+        let expected_user = User::new("public_key", "name");
+
+        assert_eq!(result, expected_user);
     }
 }
