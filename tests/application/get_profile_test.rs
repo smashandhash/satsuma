ee2dab8afe06f62196230a44b9a49e807463bb69
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-01-14 23:29:37 +0700

    Added error case for failing not founding the user profile based on the given profile_id

--- tests/application/get_profile_test.rs
+++ tests/application/get_profile_test.rs
@@ -3,24 +3,43 @@ mod tests {
     use satsuma::domain::user::User;
 
     pub trait GetProfileUseCase {
-        fn execute(&self, profile_id: String) -> User;
+        fn execute(&self, profile_id: String) -> Result<User, GetProfileUseCaseError>;
     }
 
     pub struct GetProfileUseCaseImplementation;
 
     impl GetProfileUseCase for GetProfileUseCaseImplementation {
-        fn execute(&self, _profile_id: String) -> User {
-            User::new("public_key", "name")
+        fn execute(&self, profile_id: String) -> Result<User, GetProfileUseCaseError> {
+            if profile_id == "not_found_id".to_string() {
+                return Err(GetProfileUseCaseError::ProfileNotFound)
+            }
+            Ok(User::new("public_key", "name"))
         }
     }
 
+    #[derive(Debug, Clone, PartialEq)]
+    pub enum GetProfileUseCaseError {
+        ProfileNotFound
+    }
+
     #[test]
-    fn get_profile() {
+    fn get_profile_success() {
         let profile_id = "profile_id".to_string();
         let sut = GetProfileUseCaseImplementation;
         let result = sut.execute(profile_id);
         let expected_user = User::new("public_key", "name");
 
-        assert_eq!(result, expected_user);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), expected_user);
+    }
+
+    #[test]
+    fn get_profile_failed() {
+        let profile_id = "not_found_id".to_string();
+        let sut = GetProfileUseCaseImplementation;
+        let result = sut.execute(profile_id);
+        
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), GetProfileUseCaseError::ProfileNotFound);
     }
 }
