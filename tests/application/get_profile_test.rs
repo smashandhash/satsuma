93ff11edb7eee6d4a6269e96d005a8d5146271d9
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-01-16 23:55:06 +0700

    In progress refactoring GetProfileTest file.

--- tests/application/get_profile_test.rs
+++ tests/application/get_profile_test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use satsuma::domain::user::User;
+    use rstest::rstest;
 
     pub trait GetProfileUseCase {
         fn execute(&self, public_key: String) -> Result<User, GetProfileUseCaseError>;
@@ -38,16 +39,26 @@ mod tests {
         ProfileNotFound
     }
 
-    #[test]
-    fn get_profile_success() {
-        let repository = ProfileRepositoryStub::new(None);
+    #[rstest]
+    #[test("Success", None)]
+    #[test("Failed", ProfileRepositoryError::ProfileNotFound)]
+    fn get_profile_based_on_repository(
+        #[case] _label: &str,
+        #[case] simulated_error: Option<ProfileRepositoryError>,
+        ) {
+        let repository = ProfileRepositoryStub::new(Some(simulated_error));
         let public_key = "public_key".to_string();
         let sut = GetProfileUseCaseImplementation::new(repository);
         let result = sut.execute(public_key);
         let expected_user = User::new("public_key", "name");
 
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), expected_user);
+        if let Some(simulated_error) = simulated_error {
+            assert!(result.is_err());
+            assert_eq!(result.unwrap_err(), GetProfileUseCaseError::ProfileRepositoryError(simulated_error));
+        } else {
+            assert!(result.is_ok());
+            assert_eq!(result.unwrap(), expected_user);
+        }
     }
 
     #[test]
