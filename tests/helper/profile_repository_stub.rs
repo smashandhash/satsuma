eee8178588617dca883cc79b8e4ca2ef2dbc0abe
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-01-23 23:36:18 +0700

    In progress for refactoring the ProfileRepositoryStub with an optional property for stubbed User

--- tests/helper/profile_repository_stub.rs
+++ tests/helper/profile_repository_stub.rs
@@ -8,17 +8,22 @@ use satsuma::{
 
 pub struct ProfileRepositoryStub {
     pub simulated_error: Option<ProfileRepositoryError>,
+    pub mocked_user: Option<User>,
 }
 
 impl ProfileRepositoryStub {
-    pub fn new(simulated_error: Option<ProfileRepositoryError>) -> Self {
-        Self { simulated_error }
+    pub fn new(simulated_error: Option<ProfileRepositoryError>, mocked_user: Option<User>) -> Self {
+        Self { simulated_error, mocked_user }
     }
 }
 
 impl ProfileRepository for ProfileRepositoryStub {
     fn load(&self, _public_key: String) -> Result<User, ProfileRepositoryError> {
-        self.simulated_error.clone().map_or(Ok(User::new("public_key", "name")), Err)
+        if let Some(err) = &self.simulated_error {
+            Err(err.clone())
+        } else {
+            Ok(self.mocked_user.clone().expect("It supposed to either have a simulated_error or mocked_user"))
+        }
     }
 }
 
