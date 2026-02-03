0ce9354d85e3a344f887725c7c73c41f3de60817
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-02-03 10:36:01 +0700

    Added some failed test cases if one of them is empty (GroupID, Target's public key,  Vector of Roles in String, or one of the Role in String)

--- tests/application/modify_participant_roles_test.rs
+++ tests/application/modify_participant_roles_test.rs
@@ -82,6 +82,70 @@ mod tests {
     }
 
     #[test]
+    fn group_id_empty_failed_to_modify_participant_role() {
+        let group_id = "".to_string();
+        let target_public_key = "target_public_key".to_string();
+        let previous_event_id = "previous_event_id".to_string();
+        let roles = vec!["Admin".to_string(), "Supervisor".to_string()];
+        let local_storage = LocalStorageStub::new(None);
+        let repository = UserRepositoryStub::new(None);
+        let sut = ModifyParticipantRolesUseCaseImplementation::new(local_storage, repository);
+
+        let result = sut.execute(group_id, target_public_key, roles, Some(previous_event_id));
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), ModifyParticipantRolesUseCaseError::GroupIDEmpty);
+    }
+
+    #[test]
+    fn target_public_key_empty_failed_to_modify_participant_role() {
+        let group_id = "group_id".to_string();
+        let target_public_key = "".to_string();
+        let previous_event_id = "previous_event_id".to_string();
+        let roles = vec!["Admin".to_string(), "Supervisor".to_string()];
+        let local_storage = LocalStorageStub::new(None);
+        let repository = UserRepositoryStub::new(None);
+        let sut = ModifyParticipantRolesUseCaseImplementation::new(local_storage, repository);
+
+        let result = sut.execute(group_id, target_public_key, roles, Some(previous_event_id));
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), ModifyParticipantRolesUseCaseError::TargetPublicKeyEmpty);
+    }
+
+    #[test]
+    fn assigned_roles_empty_failed_to_modify_participant_role() {
+        let group_id = "group_id".to_string();
+        let target_public_key = "target_public_key".to_string();
+        let previous_event_id = "previous_event_id".to_string();
+        let roles = Vec::new();
+        let local_storage = LocalStorageStub::new(None);
+        let repository = UserRepositoryStub::new(None);
+        let sut = ModifyParticipantRolesUseCaseImplementation::new(local_storage, repository);
+
+        let result = sut.execute(group_id, target_public_key, roles, Some(previous_event_id));
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), ModifyParticipantRolesUseCaseError::EmptyAssignedRoles);
+    }
+
+    #[test]
+    fn some_role_is_empty_failed_to_modify_participant_role() {
+        let group_id = "group_id".to_string();
+        let target_public_key = "target_public_key".to_string();
+        let previous_event_id = "previous_event_id".to_string();
+        let roles = vec!["Admin".to_string(), "".to_string()];
+        let local_storage = LocalStorageStub::new(None);
+        let repository = UserRepositoryStub::new(None);
+        let sut = ModifyParticipantRolesUseCaseImplementation::new(local_storage, repository);
+
+        let result = sut.execute(group_id, target_public_key, roles, Some(previous_event_id));
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), ModifyParticipantRolesUseCaseError::RoleIsEmpty);
+    }
+
+    #[test]
     fn local_storage_error_failed_to_modify_participant_role() {
         let group_id = "group_id".to_string();
         let target_public_key = "target_public_key".to_string();
