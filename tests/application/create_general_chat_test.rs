f2175fafb70885869b54600ff044801b03131899
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-03-03 22:54:17 +0700

    In progress for creating the success case of creating a general chat. In the next commit, considering to change the name into group chat instead of general chat.

--- tests/application/create_general_chat_test.rs
+++ tests/application/create_general_chat_test.rs
@@ -1,1 +1,8 @@
-use satsuma::domain::User;
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn successfully_create_a_general_chat() {
+        let sender_public_key = "sender_public_key".to_string();
+        let member_public_keys = vec!["first_member_key".to_string(), "second_member_key".to_string()];
+    }
+}
