d875643f514d98b612c1009c34b329fd1509b9cd
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-03-06 23:32:57 +0700

    In progress for implementing the success case of creating the group chat, by creating the trait, struct, and implementation of CreateGroupChatUseCase.

--- tests/application/create_general_chat_test.rs
+++ tests/application/create_general_chat_test.rs
@@ -1,12 +1,36 @@
 #[cfg(test)]
 mod tests {
-    use satsuma::domain::chat_container::{
-        ChatContainer,
-        ChatContainerContext,
-        ChatContainerGroupType,
+    use satsuma::{
+        domain::chat_container::{
+            ChatContainer,
+            ChatContainerContext,
+            ChatContainerGroupType,
+        },
+        infrastructure::chat_container_repository::ChatContainerRepository,
     };
     use crate::helper::chat_container_repository_stub::ChatContainerRepositoryStub;
     use std::sync::Arc;
+
+    pub trait CreateGroupChatUseCase {
+        fn execute(&self, creator_public_key: &str, admins_public_key: Vec<&str>, member_public_keys: Vec<&str>) -> ChatContainer;
+    }
+
+    pub struct CreateGroupChatUseCaseImplementation<R: ChatContainerRepository> {
+        repository: Arc<R>
+    }
+
+    impl<R: ChatContainerRepository> CreateGroupChatUseCaseImplementation<R> {
+        pub fn new(repository: Arc<R>) -> Self {
+            Self {
+                repository
+            }
+        }
+    }
+
+    impl<R: ChatContainerRepository> CreateGroupChatUseCase for CreateGroupChatUseCaseImplementation {
+        fn execute(&self, creator_public_key: &str, admins_public_key: Vec<&str>, member_public_keys: Vec<&str>) -> ChatContainer {
+        }
+    }
 
     #[test]
     fn successfully_create_a_general_chat() {
@@ -17,8 +41,8 @@ mod tests {
             "id".to_string(),
             ChatContainerContext::Group {
                 group_type: ChatContainerGroupType::Channel,
-                creator_public_key: creator_public_key,
-                admins_public_key: admins_public_key
+                creator_public_key,
+                admins_public_key
             },
             member_public_keys.clone(),
         );
