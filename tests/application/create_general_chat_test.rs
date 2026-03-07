e0067f30500299adf8935552731173d23c156600
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-03-07 23:29:25 +0700

    In progress for handling the creation of ChatContainerID.

--- tests/application/create_general_chat_test.rs
+++ tests/application/create_general_chat_test.rs
@@ -6,13 +6,16 @@ mod tests {
             ChatContainerContext,
             ChatContainerGroupType,
         },
-        infrastructure::chat_container_repository::ChatContainerRepository,
+        infrastructure::chat_container_repository::{
+            ChatContainerRepository,
+            ChatContainerRepositoryError,
+        },
     };
     use crate::helper::chat_container_repository_stub::ChatContainerRepositoryStub;
     use std::sync::Arc;
 
     pub trait CreateGroupChatUseCase {
-        fn execute(&self, creator_public_key: &str, admins_public_key: Vec<&str>, member_public_keys: Vec<&str>) -> ChatContainer;
+        fn execute(&self, creator_public_key: &str, admins_public_key: Vec<&str>, member_public_keys: Vec<&str>) -> Result<ChatContainer, CreateGroupChatUseCaseError>;
     }
 
     pub struct CreateGroupChatUseCaseImplementation<R: ChatContainerRepository> {
@@ -25,11 +28,27 @@ mod tests {
                 repository
             }
         }
+
+        fn generate_chat_session_id(&self, sender_public_key: &str, recipient_public_key: &str) -> String {
+            let mut keys = vec![sender_public_key.to_string(), recipient_public_key.to_string()];
+            keys.sort();
+
+            let joined_keys = format!("{}:{}", keys[0], keys[1]);
+            format!("{:x}", compute(joined_keys))
+        }
     }
 
     impl<R: ChatContainerRepository> CreateGroupChatUseCase for CreateGroupChatUseCaseImplementation {
-        fn execute(&self, creator_public_key: &str, admins_public_key: Vec<&str>, member_public_keys: Vec<&str>) -> ChatContainer {
+        fn execute(&self, creator_public_key: &str, admins_public_key: Vec<&str>, member_public_keys: Vec<&str>) -> Result<ChatContainer, CreateGroupChatUseCaseError> {
+            let chat_container = ChatContainer::new(
+                self.generate_chat_session_id(creator_public_key, member_public_keys)
+            self.repository.save(
         }
+    }
+
+    #[derive(Debug, Clone, PartialEq)]
+    pub enum CreateGroupChatUseCaseError {
+        RepositoryError(ChatContainerRepositoryError)
     }
 
     #[test]
