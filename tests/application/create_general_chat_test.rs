918e5d6eb2b7fc34d35f3ed85b1db0575f11cbe7
Gogo <226705116+smashandhash@users.noreply.github.com>
2026-03-08 23:33:47 +0700

    Completed test case for creating a group chat in success phase.

--- tests/application/create_general_chat_test.rs
+++ tests/application/create_general_chat_test.rs
@@ -12,6 +12,7 @@ mod tests {
         },
     };
     use crate::helper::chat_container_repository_stub::ChatContainerRepositoryStub;
+    use md5::compute;
     use std::sync::Arc;
 
     pub trait CreateGroupChatUseCase {
@@ -29,8 +30,8 @@ mod tests {
             }
         }
 
-        fn generate_chat_session_id(&self, sender_public_key: &str, recipient_public_key: &str) -> String {
-            let mut keys = vec![sender_public_key.to_string(), recipient_public_key.to_string()];
+        fn generate_chat_session_id(&self, member_public_keys: Vec<&str>) -> String {
+            let mut keys: Vec<String> = member_public_keys.into_iter().map(|v| v.to_string()).collect();
             keys.sort();
 
             let joined_keys = format!("{}:{}", keys[0], keys[1]);
@@ -38,11 +39,22 @@ mod tests {
         }
     }
 
-    impl<R: ChatContainerRepository> CreateGroupChatUseCase for CreateGroupChatUseCaseImplementation {
+    impl<R: ChatContainerRepository> CreateGroupChatUseCase for CreateGroupChatUseCaseImplementation<R> {
         fn execute(&self, creator_public_key: &str, admins_public_key: Vec<&str>, member_public_keys: Vec<&str>) -> Result<ChatContainer, CreateGroupChatUseCaseError> {
             let chat_container = ChatContainer::new(
-                self.generate_chat_session_id(creator_public_key, member_public_keys)
-            self.repository.save(
+                self.generate_chat_session_id(member_public_keys.clone()),
+                ChatContainerContext::Group {
+                    group_type: ChatContainerGroupType::Channel,
+                    creator_public_key: creator_public_key.to_string(),
+                    admins_public_key: admins_public_key.into_iter().map(|v| v.to_string()).collect(),
+                },
+                member_public_keys.into_iter().map(|v| v.to_string()).collect()
+            );
+            self.repository
+                .save(chat_container.clone())
+                .map_err(|e| CreateGroupChatUseCaseError::RepositoryError(e))?;
+
+            Ok(chat_container)
         }
     }
 
@@ -53,22 +65,28 @@ mod tests {
 
     #[test]
     fn successfully_create_a_general_chat() {
-        let creator_public_key = "sender_public_key".to_string();
-        let admins_public_key = vec![creator_public_key.clone()];
-        let member_public_keys = vec![creator_public_key.clone(), "first_member_key".to_string(), "second_member_key".to_string()];
+        let creator_public_key = "sender_public_key";
+        let admins_public_key = vec![creator_public_key];
+        let member_public_keys = vec![creator_public_key, "first_member_key", "second_member_key"];
         let chat_container = ChatContainer::new(
             "id".to_string(),
             ChatContainerContext::Group {
                 group_type: ChatContainerGroupType::Channel,
-                creator_public_key,
-                admins_public_key
+                creator_public_key: creator_public_key.to_string(),
+                admins_public_key: admins_public_key.clone().into_iter().map(|k| k.to_string()).collect()
             },
-            member_public_keys.clone(),
+            member_public_keys.clone().into_iter().map(|k| k.to_string()).collect(),
         );
         let repository = Arc::new(ChatContainerRepositoryStub::new(
                 None,
                 Some(chat_container.clone())
         ));
         let sut = CreateGroupChatUseCaseImplementation::new(repository);
+
+        let result = sut.execute(creator_public_key, admins_public_key, member_public_keys);
+        let given_chat_container = result.unwrap();
+
+        assert_eq!(given_chat_container.context, chat_container.context);
+        assert_eq!(given_chat_container.participant_public_keys, chat_container.participant_public_keys);
     }
 }
